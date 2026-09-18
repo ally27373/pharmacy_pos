c16286a208d47284a6df690640037eeb283fd56b
DyNgPinasOfficial <arjanel.dy@gmail.com>
2026-09-18 23:44:08 +0800

    test: login brand on deployment

--- app/auth/login.php
+++ app/auth/login.php
@@ -72,7 +72,7 @@ require_once '../../includes/header.php';
 
                 <h5 class="pharmacy-name">
 
-                    NICA XANDRA PHARMACY
+                    NICA X4NDRA PHARMACY
 
                 </h5>
 
